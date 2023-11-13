cad37dfa2b6ab9be957fb4d023a37f0eb44abd35
s3xmaxman <badside3919@gmail.com>
2023-11-13 14:32:12 +0900

    add rightsidebar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,17 +3,21 @@ import { Outlet } from "react-router-dom";
 import Topbar from "@/components/shared/Topbar";
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
 
 const RootLayout = () => {
   return (
     <div className="w-full md:flex">
       <Topbar />
       <LeftSidebar />
+      
+
 
       <section className="flex flex-1 h-full">
         <Outlet />
       </section>
 
+      <RightSidebar />
       <Bottombar />
     </div>
   );
---
+++ src/components/shared/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <nav className="rightsidebar">
+      <h2 className='h3-bold md:h2-bold w-full '>おすすめ</h2>
+    </nav>
+  )
+}
+
+export default RightSidebar
