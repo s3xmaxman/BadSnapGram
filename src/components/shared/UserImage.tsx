69a42a230c0eb77ab4211167d7e52998b1d1b250
s3xmaxman <badside3919@gmail.com>
2023-11-15 22:03:46 +0900

    fix video fullscreen

--- src/components/shared/UserImage.tsx
+++ src/components/shared/UserImage.tsx
@@ -6,13 +6,12 @@ type UserImageProps = {
     user: Models.Document;
  };
 
- 
- 
+
  const UserImage = ({ user }: UserImageProps) => {
     return (
         <Link to={`/profile/${user.$id}`}>
             <div className="flex flex-col items-center space-y-4">
-                <div className="rounded-full overflow-hidden w-14 h-14">
+                <div className="rounded-full overflow-hidden w-14 h-14 border-4 border-[#3121FF] myImage">
                     <img 
                         src={user.imageUrl || "/assets/icons/profile-placeholder.svg" } 
                         alt="creator"
@@ -21,9 +20,10 @@ type UserImageProps = {
                 </div>
             </div>
         </Link> 
+
     )
  }
+
  
-   
  export default UserImage
  
